d5ce15394d3add66461a66bf0c88b8cff20f4456
FLCorporation <108215519+FLCorp@users.noreply.github.com>
2024-03-01 12:24:29 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,294 @@
+//Popup 1 Axolotl
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showPopupButton = document.getElementById('showPopupButton');
+    var popup = document.getElementById('popup');
+    var closeButton = document.getElementById('closeButton');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showPopupButton.addEventListener('click', function() {
+      popup.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton.addEventListener('click', function() {
+      popup.style.display = 'none';
+    });
+  });
+
+//Popup 2 Maison Axolotl
+  document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showSecondPopupButton = document.getElementById('showSecondPopupButton');
+    var popup2 = document.getElementById('popup2');
+    var closeButton2 = document.getElementById('closeButton2');
+    var closeButtonTwo = document.getElementById('closeButtonTwo');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showSecondPopupButton.addEventListener('click', function() {
+      popup2.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton2.addEventListener('click', function() {
+      popup2.style.display = 'none';
+    });
+    closeButtonTwo.addEventListener('click', function() {
+      popup2.style.display = 'none';
+    });
+  });
+  
+//Popup 3 Fontaine
+  document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showThirdPopupButton = document.getElementById('showThirdPopupButton');
+    var popup3 = document.getElementById('popup3');
+    var closeButton3 = document.getElementById('closeButton3');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showThirdPopupButton.addEventListener('click', function() {
+      popup3.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton3.addEventListener('click', function() {
+      popup3.style.display = 'none';
+    });
+  });
+  
+//Popup 4 Cerisier
+  document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showFourthPopupButton = document.getElementById('showFourthPopupButton');
+    var popup4 = document.getElementById('popup4');
+    var closeButton4 = document.getElementById('closeButton4');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showFourthPopupButton.addEventListener('click', function() {
+      popup4.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton4.addEventListener('click', function() {
+      popup4.style.display = 'none';
+    });
+  });
+  
+//Popup 5 Arbre Nether
+  document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showFifthPopupButton = document.getElementById('showFifthPopupButton');
+    var popup5 = document.getElementById('popup5');
+    var closeButton5 = document.getElementById('closeButton5');
+    var closeButtonFive = document.getElementById('closeButtonFive');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showFifthPopupButton.addEventListener('click', function() {
+      popup5.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton5.addEventListener('click', function() {
+      popup5.style.display = 'none';
+    });
+    closeButtonFive.addEventListener('click', function() {
+      popup5.style.display = 'none';
+    });
+  });
+  
+//Popup 6 Manoir Médiéval
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showSixthPopupButton = document.getElementById('showSixthPopupButton');
+    var popup6 = document.getElementById('popup6');
+    var closeButton6 = document.getElementById('closeButton6');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showSixthPopupButton.addEventListener('click', function() {
+      popup6.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton6.addEventListener('click', function() {
+      popup6.style.display = 'none';
+    });
+  });
+  
+//Popup 7 Appartements Moderne
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showSeventhPopupButton = document.getElementById('showSeventhPopupButton');
+    var popup7 = document.getElementById('popup7');
+    var closeButton7 = document.getElementById('closeButton7');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showSeventhPopupButton.addEventListener('click', function() {
+      popup7.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton7.addEventListener('click', function() {
+      popup7.style.display = 'none';
+    });
+  });
+   
+//Popup 8 Maison Moderne
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showEighthPopupButton = document.getElementById('showEighthPopupButton');
+    var popup8 = document.getElementById('popup8');
+    var closeButton8 = document.getElementById('closeButton8');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showEighthPopupButton.addEventListener('click', function() {
+      popup8.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton8.addEventListener('click', function() {
+      popup8.style.display = 'none';
+    });
+  });
+    
+//Popup 9 Grande Maison Moderne
+document.addEventListener('DOMContentLoaded', function() {
+    // Sélectionner le bouton et la popup
+    var showNinethPopupButton = document.getElementById('showNinethPopupButton');
+    var popup9 = document.getElementById('popup9');
+    var closeButton9 = document.getElementById('closeButton9');
+  
+    // Afficher la popup lorsque le bouton est cliqué
+    showNinethPopupButton.addEventListener('click', function() {
+      popup9.style.display = 'block';
+    });
+  
+    // Fermer le popup lorsque le bouton de fermeture est cliqué
+    closeButton9.addEventListener('click', function() {
+      popup9.style.display = 'none';
+    });
+  });
+
+      
+//Popup 10 Maison Moderne
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showTenthPopupButton = document.getElementById('showTenthPopupButton');
+  var popup10 = document.getElementById('popup10');
+  var closeButton10 = document.getElementById('closeButton10');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showTenthPopupButton.addEventListener('click', function() {
+    popup10.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton10.addEventListener('click', function() {
+    popup10.style.display = 'none';
+  });
+});
+
+//Popup 11 Paterne de Sol 1
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showEleventhPopupButton = document.getElementById('showEleventhPopupButton');
+  var popup11 = document.getElementById('popup11');
+  var closeButton11 = document.getElementById('closeButton11');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showEleventhPopupButton.addEventListener('click', function() {
+    popup11.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton11.addEventListener('click', function() {
+    popup11.style.display = 'none';
+  });
+});
+
+//Popup 12 Paterne de Sol 1 Style 2
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showTwelthPopupButton = document.getElementById('showTwelthPopupButton');
+  var popup12 = document.getElementById('popup12');
+  var closeButton12 = document.getElementById('closeButton12');
+  var closeButtonTwelth = document.getElementById('closeButtonTwelth');
+  var closeButtonTwelth1 = document.getElementById('closeButtonTwelth1');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showTwelthPopupButton.addEventListener('click', function() {
+  popup12.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton12.addEventListener('click', function() {
+    popup12.style.display = 'none';
+  });
+  closeButtonTwelth.addEventListener('click', function() {
+    popup12.style.display = 'none';
+  });
+  closeButtonTwelth1.addEventListener('click', function() {
+    popup12.style.display = 'none';
+  });
+});
+
+//Popup 13 Paterne de Sol 1 Style 3
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showThirteenthPopupButton = document.getElementById('showThirteenthPopupButton');
+  var popup13 = document.getElementById('popup13');
+  var closeButton13 = document.getElementById('closeButton13');
+  var closeButtonThirteenth = document.getElementById('closeButtonThirteenth');
+  var showTwelthPopupButton1 = document.getElementById('closeButtonThirteenth1');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showThirteenthPopupButton.addEventListener('click', function() {
+  popup13.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton13.addEventListener('click', function() {
+    popup13.style.display = 'none';
+  });
+  closeButtonThirteenth.addEventListener('click', function() {
+    popup13.style.display = 'none';
+  });
+  closeButtonThirteenth1.addEventListener('click', function() {
+    popup13.style.display = 'none';
+  });
+});
+
+//Popup 14 Paterne de Sol 2
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showFourteenthPopupButton = document.getElementById('showFourteenthPopupButton');
+  var popup14 = document.getElementById('popup14');
+  var closeButton14 = document.getElementById('closeButton14');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showFourteenthPopupButton.addEventListener('click', function() {
+    popup14.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton14.addEventListener('click', function() {
+    popup14.style.display = 'none';
+  });
+});
+
+//Popup 15 Paterne de Sol 2 Style 2
+document.addEventListener('DOMContentLoaded', function() {
+  // Sélectionner le bouton et la popup
+  var showFithteenPopupButton = document.getElementById('showFithteenPopupButton');
+  var popup15 = document.getElementById('popup15');
+  var closeButton15 = document.getElementById('closeButton15');
+
+  // Afficher la popup lorsque le bouton est cliqué
+  showFithteenPopupButton.addEventListener('click', function() {
+    popup15.style.display = 'block';
+  });
+
+  // Fermer le popup lorsque le bouton de fermeture est cliqué
+  closeButton15.addEventListener('click', function() {
+    popup15.style.display = 'none';
+  });
+});
